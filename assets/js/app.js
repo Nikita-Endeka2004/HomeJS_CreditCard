bd7c993f5d9cb544c8401ceae2b8a5e7634da64c
Nikita Endea <90311455+Nikita-Endeka2004@users.noreply.github.com>
2022-09-19 19:02:12 +0300

    js

---
+++ assets/js/app.js
@@ -0,0 +1,66 @@
+
+function FullCardInfo(){
+
+    let card = numberInput.value;
+
+    let test = false;
+
+    let result;
+
+    card = [...card]
+            .reverse()
+            .map((item) => +item);
+
+    let sum = 0;
+
+    for(let i = 0; i < card.length; i++){
+        if(i % 2 == 0){
+            sum = sum + card[i];
+        }else {
+            let n = card[i] * 2;
+            sum = sum + (n > 9 ? n - 9 : n);
+        }
+    }
+
+    if((sum % 10) == 0){
+
+        test = true;
+
+        AnswerInput.innerHTML = `YES`;
+
+    }else{
+
+        test = false;
+
+        AnswerInput.innerHTML = `NO`;
+
+    }
+
+    if(test == true){
+
+        if (/^5[1-5]/.test(card)) {
+
+            result = 'mastercard';
+
+          } else if (/^4/.test(card)) {
+
+            result = 'visa';
+
+          } else if ( /^(5018|5020|5038|6304|6759|676[1-3])/.test(card)) {
+
+            result = 'maestro';
+        
+          } else {
+            
+            result = 'unknown'
+          }
+
+          TypeCard.innerHTML = `${result}`;
+
+    }else{
+
+        TypeCard.innerHTML = `Not found`;
+
+    }
+
+}
